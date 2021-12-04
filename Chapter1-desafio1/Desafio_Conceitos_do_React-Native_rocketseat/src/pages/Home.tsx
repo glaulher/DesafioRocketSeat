799ca88957cfe40b2430897978866061136ab455
glaulher <glaulher.developer@gmail.com>
2021-12-04 11:15:08 -0300

    update challenge chapter1

--- Chapter1-desafio1/Desafio_Conceitos_do_React-Native_rocketseat/src/pages/Home.tsx
+++ Chapter1-desafio1/Desafio_Conceitos_do_React-Native_rocketseat/src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Alert, StyleSheet, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
 import { Header } from '../components/Header';
 import { Task, TasksList } from '../components/TasksList';
@@ -11,27 +11,12 @@ export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    const taskState = tasks.map(task => ({ ...task }));
-
-    const findTask = (taskState.find(task=>task.title === newTaskTitle))
-
-
-    if(findTask){
-      Alert.alert(
-        "Task já cadastrada",
-        "Você não pode cadastrar uma task com o mesmo nome"
-      );
-      return
-    }
-    
-
     const addTask ={
       id: new Date().getTime(),
       title: newTaskTitle,
       done: false,
     }
     setTasks(oldTasks =>[...oldTasks,addTask])
-
   }
 
   function handleToggleTaskDone(id: number) {
@@ -50,38 +35,8 @@ export function Home() {
     
   }
 
-  function handleEditTask(taskId: number,taskNewTitle: string){
-    const updatedTasks = tasks.map(task => ({ ...task }))
-    
-    const findTask = updatedTasks.find(task=>task.id === taskId)
-
-    if(!findTask) {
-      return
-    }
-    findTask.title = taskNewTitle    
-    setTasks(updatedTasks)   
-    
-  }
-
   function handleRemoveTask(id: number) {
-    
-    Alert.alert(
-      "Remover item",
-      "Tem certeza que você deseja remover esse item?",
-      [
-        {
-          text: "Não",
-        },
-        {
-          text: "Sim",
-          onPress: () => {
-            setTasks(oldState => oldState.filter(task => task.id !== id));
-            return;
-          },
-        },
-      ]
-    );
-
+    setTasks(oldState => oldState.filter(task => task.id !== id));
   }
 
   return (
@@ -94,7 +49,6 @@ export function Home() {
         tasks={tasks} 
         toggleTaskDone={handleToggleTaskDone}
         removeTask={handleRemoveTask} 
-        editTask={handleEditTask}
       />
     </View>
   )
